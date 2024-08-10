4980bfdd06b44432036de14e034698d82d02b16f
vin <vinayak.verma@gmail.com>
2024-08-11 00:01:11 +0530

    first-commit

---
+++ account/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+def home(request):
+    return render(request, 'home.html')
